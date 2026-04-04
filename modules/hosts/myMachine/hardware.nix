03104ebfbb64c19ddba09f85575d040e54837e1b
Koye64 <me@koye64.com>
2026-04-03 23:42:06 -0700

    change hardware

--- modules/hosts/myMachine/hardware.nix
+++ modules/hosts/myMachine/hardware.nix
@@ -1,64 +1,67 @@
 { self, inputs, ... }: {
-  flake.nixosModules.myMachineHardware = { pkgs, lib, modulesPath, ... }: {
-    imports =
-      [ (modulesPath + "/profiles/qemu-guest.nix")
-      ];
-  
+
+  flake.nixosModules.myMachineHardware = { config, lib, pkgs, modulesPath, ... }: {
+    imports = [
+      (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
     boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
     boot.initrd.kernelModules = [ ];
-    boot.kernelModules = [ "kvm-intel" ];
+    boot.kernelModules = [ "kvm-amd" ];
     boot.extraModulePackages = [ ];
-  
+
     fileSystems."/" =
-      { device = "/dev/disk/by-uuid/39a3fa45-430a-49ee-97d8-ac3c75ac9138";
+      { device = "/dev/disk/by-uuid/034cc913-5fd8-4291-b779-61dcd3271925";
         fsType = "btrfs";
-        options = [ "subvol=@" ];
+        options = [ "compress=zstd" "subvol=@" ];
       };
-  
+
     fileSystems."/home" =
-      { device = "/dev/disk/by-uuid/39a3fa45-430a-49ee-97d8-ac3c75ac9138";
+      { device = "/dev/disk/by-uuid/034cc913-5fd8-4291-b779-61dcd3271925";
         fsType = "btrfs";
         options = [ "compress=zstd" "subvol=@home" ];
       };
-  
+
     fileSystems."/root" =
-      { device = "/dev/disk/by-uuid/39a3fa45-430a-49ee-97d8-ac3c75ac9138";
+      { device = "/dev/disk/by-uuid/034cc913-5fd8-4291-b779-61dcd3271925";
         fsType = "btrfs";
         options = [ "compress=zstd" "subvol=@root" ];
       };
-  
+
     fileSystems."/srv" =
-      { device = "/dev/disk/by-uuid/39a3fa45-430a-49ee-97d8-ac3c75ac9138";
+      { device = "/dev/disk/by-uuid/034cc913-5fd8-4291-b779-61dcd3271925";
         fsType = "btrfs";
         options = [ "compress=zstd" "subvol=@srv" ];
       };
-  
+
     fileSystems."/var/cache" =
-      { device = "/dev/disk/by-uuid/39a3fa45-430a-49ee-97d8-ac3c75ac9138";
+      { device = "/dev/disk/by-uuid/034cc913-5fd8-4291-b779-61dcd3271925";
         fsType = "btrfs";
         options = [ "compress=zstd" "subvol=@cache" ];
       };
-  
+
     fileSystems."/var/log" =
-      { device = "/dev/disk/by-uuid/39a3fa45-430a-49ee-97d8-ac3c75ac9138";
+      { device = "/dev/disk/by-uuid/034cc913-5fd8-4291-b779-61dcd3271925";
         fsType = "btrfs";
         options = [ "compress=zstd" "subvol=@log" ];
       };
-  
+
     fileSystems."/var/tmp" =
-      { device = "/dev/disk/by-uuid/39a3fa45-430a-49ee-97d8-ac3c75ac9138";
+      { device = "/dev/disk/by-uuid/034cc913-5fd8-4291-b779-61dcd3271925";
         fsType = "btrfs";
         options = [ "compress=zstd" "subvol=@tmp" ];
       };
-  
+
     fileSystems."/boot" =
-      { device = "/dev/disk/by-uuid/35C3-4A66";
+      { device = "/dev/disk/by-uuid/CB6D-7525";
         fsType = "vfat";
         options = [ "fmask=0022" "dmask=0022" ];
       };
-  
+
     swapDevices = [ ];
-  
+
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
   };
+
 }
