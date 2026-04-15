b455f3e5f49c50f060bfdce3ccde26294ebd65af
Koye64 <me@koye64.com>
2026-04-15 01:14:19 -0700

    reorder home manager in host

--- modules/hosts/boy-deer/default.nix
+++ modules/hosts/boy-deer/default.nix
@@ -4,4 +4,15 @@
       self.nixosModules.boyDeerConfiguration
     ];
   };
+
+  flake.homeConfigurations.koye = inputs.home-manager.lib.homeManagerConfiguration {
+    # pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+    modules = [
+      self.homeModules.koyeModule
+      {
+        home.username = "koye";
+        home.homeDirectory = "/home/koye";
+      }
+    ];
+  };
 }
