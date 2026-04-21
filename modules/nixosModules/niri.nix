aa4d2da78dd5eb0cd8bdcc1e1120d0f35962e6fa
Koye64 <me@koye64.com>
2026-04-20 20:44:37 -0700

    adjust xdg portal defaults

--- modules/nixosModules/niri.nix
+++ modules/nixosModules/niri.nix
@@ -4,6 +4,13 @@
       inputs.niri.overlays.niri
     ];
 
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      config = {
+        common.default = [ "gnome" ];
+      };
+    };
     services.dbus.implementation = "broker";
     services.udisks2.enable = true;
     services.power-profiles-daemon.enable = true;
