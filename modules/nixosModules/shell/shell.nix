f18be8237c5e9d2a0e435f3125a0fa48a2ed6ecc
Koye64 <me@koye64.com>
2026-04-21 02:19:31 -0700

    add starship

---
+++ modules/nixosModules/shell/shell.nix
@@ -0,0 +1,30 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.shell = { pkgs, lib, ... }: {
+    programs.zsh.enable = true;
+    users.users.koye.shell = pkgs.zsh;
+    environment.variables = {
+      # EDITOR = lib.getExe self'.packages.${pkgs.stdenv.hostPlatform.system}.neovim;
+      # VISUAL = lib.getExe self'.packages.${pkgs.stdenv.hostPlatform.system}.neovim;
+      EDITOR = lib.getExe pkgs.neovim;
+      VISUAL = lib.getExe pkgs.neovim;
+    };
+
+    programs.zsh = {
+      syntaxHighlighting.enable = true;
+      autosuggestions = {
+        enable = true;
+        strategy = [ "completion" "history" ];
+      };
+      enableBashCompletion = true;
+    };
+
+    programs.starship = {
+      enable = true;
+      transientPrompt.enable = true;
+      settings = (builtins.fromTOML
+        (builtins.readFile ./starship.toml)
+      );
+      presets = [ "nerd-font-symbols" "pure-preset" ];
+    };
+  };
+}
