b4d0af24be3b9f150dd10fce1e90c2acdfb37ae1
Koye64 <me@koye64.com>
2026-04-29 23:36:03 -0700

    enable gnome keyring

--- modules/nixosModules/niri.nix
+++ modules/nixosModules/niri.nix
@@ -16,6 +16,10 @@
     services.power-profiles-daemon.enable = true;
     services.upower.enable = true;
 
+    services.gnome.gnome-keyring.enable = true;
+    security.pam.services.greetd.enableGnomeKeyring = true;
+    programs.seahorse.enable = true;
+
     programs.niri.enable = true;
     programs.niri.package = pkgs.niri-unstable;
 
