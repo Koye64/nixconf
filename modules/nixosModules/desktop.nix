45a4df36b2ef71be93a0a537206ca40700ccb553
Koye64 <me@koye64.com>
2026-05-10 20:00:46 -0700

    add obs modules

--- modules/nixosModules/desktop.nix
+++ modules/nixosModules/desktop.nix
@@ -4,6 +4,11 @@
       obs-studio.enable = true;
       obs-studio.enableVirtualCamera = true;
       obs-studio.plugins = with pkgs.obs-studio-plugins; [
+        wlrobs
+        obs-backgroundremoval
+        obs-vaapi
+        obs-gstreamer
+        obs-vkcapture
         obs-pipewire-audio-capture
       ];
     };
