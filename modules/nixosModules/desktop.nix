ed542f812a5e9038525588ff1141ba7bae3dff80
Koye64 <me@koye64.com>
2026-05-10 19:48:10 -0700

    add pipewire audio capture to OBS

--- modules/nixosModules/desktop.nix
+++ modules/nixosModules/desktop.nix
@@ -3,6 +3,9 @@
     programs = {
       obs-studio.enable = true;
       obs-studio.enableVirtualCamera = true;
+      obs-studio.plugins = with pkgs.obs-studio-plugins; [
+        obs-pipewire-audio-capture
+      ];
     };
 
     environment.systemPackages = [
