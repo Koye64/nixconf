655f561f7d53f4b5fc2bde0a7b1f6b53537a9d16
Koye64 <me@koye64.com>
2026-06-06 15:49:47 -0700

    switch to discord canary for screenshare

--- modules/nixosModules/vencord.nix
+++ modules/nixosModules/vencord.nix
@@ -1,7 +1,7 @@
 { self, inputs, ... }: {
   flake.nixosModules.vencord = { pkgs, lib, ... }: {
     environment.systemPackages = [
-      (pkgs.discord.override {
+      (pkgs.discord-canary.override {
         withVencord = true;
       })
     ];
